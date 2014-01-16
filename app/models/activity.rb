9853f5404380ae2812853609a84ccca8f10bc969
Eddie Lee <eddieberklee@gmail.com>
2014-01-16 03:08:01 -0800

    fixed an error with not checking for today's date with activity.rb#today_log

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,7 +4,14 @@ class Activity < ActiveRecord::Base
 
   def today_log
     ordered = self.ordered_logs
-    ordered[-1]
+    l = ordered[-1]
+    if l
+      if l.created_at.to_date == Time.now.to_date
+        l
+      else
+        nil
+      end
+    end
   end
 
   def ordered_logs
